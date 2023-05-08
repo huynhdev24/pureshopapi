06a331e1bc90ee7ed277eed3f1f0144c96a12a1b
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-08 10:27:12 +0700

    add OrderAggregate Entities

---
+++ pureshopapi/Core/Entities/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace pureshopapi.Core.Entities.OrderAggregate
+{
+    public enum OrderStatus
+    {
+        [EnumMember(Value  = "Pending")]
+        Pending,
+
+        [EnumMember(Value = "PaymentReceived")]
+        PaymentReceived,
+
+        [EnumMember(Value = "PaymentFailed")]
+        PaymentFailed
+    }
+}
